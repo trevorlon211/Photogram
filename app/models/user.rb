7eabccab6ce11bde05a9ab2bab490be52dbfe21f
trevorlon211 <trevorlon.211@gmail.com>
2016-05-11 23:27:30 -0400

    Registrain&login page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
  
    devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+         
+  has_attached_file :avatar, styles: { medium: '152x152#' }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
