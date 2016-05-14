f6ef56bf194fc528a13dc4328500fd527388d652
trevorlon211 <trevorlon.211@gmail.com>
2016-05-14 16:36:34 -0400

    Part2

--- Gemfile
+++ Gemfile
@@ -14,10 +14,7 @@ gem 'bootstrap-sass', '~> 3.3.6'
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.1.0'
-# See https://github.com/rails/execjs#readme for more supported runtimes
-# gem 'therubyracer', platforms: :ruby
 
-# Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
@@ -35,6 +32,7 @@ gem 'simple_form'
 
 gem 'devise', '~> 3.5.8'
 
+gem 'kaminari', '~> 0.16.3'  
 
 
 # Use ActiveModel has_secure_password
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,43 +1,52 @@
 class CommentsController < ApplicationController
   before_action :set_post
 
-	  def create
-      @comment = @post.comments.build(comment_params)
-      @comment.user_id = current_user.id
+  def index
+    @comments = @post.comments.order("created_at ASC")
 
-      if @comment.save
-        respond_to do |format|
-          format.html { redirect_to root_path }
-          format.js
-        end
-      else
-        flash[:alert] = "Check the comment form, something went wrong."
-        render root_path
+    respond_to do |format|
+      format.html { render layout: !request.xhr? }
+    end
+  end
+
+
+  def create
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+    else
+      flash[:alert] = "Check the comment form, something went wrong."
+      render root_path
+    end
+  end
+
+  def destroy  
+    @comment = @post.comments.find(params[:id])  
+    if @comment.user_id == current_user.id
+      @comment.delete
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        forat.js
       end
     end
-
-    def destroy  
-      @comment = @post.comments.find(params[:id])  
-      if @comment.user_id == current_user.id
-        @comment.delete
-        respond_to do |format|
-          format.html { redirect_to root_path }
-          forat.js
-        end
-      end
-    end  
+  end  
 
 
 
 
-    private
+  private
 
-    def comment_params  
-      params.require(:comment).permit(:content)
-    end
+  def comment_params  
+    params.require(:comment).permit(:content)
+  end
 
-    def set_post  
-      @post = Post.find(params[:post_id])
-    end  
+  def set_post  
+    @post = Post.find(params[:post_id])
+  end  
 
 end
