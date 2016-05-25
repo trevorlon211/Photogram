a4928e5aae89582885f0d17495a6426602a3941d
trevorlon211 <trevorlon.211@gmail.com>
2016-05-25 12:20:11 -0400

    Part5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
-class User < ActiveRecord::Base
-   has_many :posts, dependent: :destroy  
-   has_many :comments, dependent: :destroy
+class User < ActiveRecord::Base  
+  acts_as_voter
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :user_name, presence: true, length: { minimum: 4, maximum: 12 }
 
-   validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }  
- 
-   devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         
-  
-    has_attached_file :avatar, styles: { medium: '152x152#' }
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  has_attached_file :avatar, styles: { medium: '152x152#' }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
-end
+end 
